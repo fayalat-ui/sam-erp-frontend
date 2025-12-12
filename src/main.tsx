64532945e1db37632a1754b9ec411e4caffd07ec
fayalat-ui <fayalat@gmail.com>
2025-12-12 18:44:17 -0300

    Replace initMsal with msalInstance initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { initMsal } from "./auth/initMsal";
+import { msalInstance } from "./auth/msalInstance";
 
 (async () => {
-  await initMsal();
+  await msalInstance.initialize();
+
+  const result = await msalInstance.handleRedirectPromise();
+  if (result?.account) {
+    msalInstance.setActiveAccount(result.account);
+  } else {
+    const accounts = msalInstance.getAllAccounts();
+    if (accounts.length > 0) msalInstance.setActiveAccount(accounts[0]);
+  }
+
   createRoot(document.getElementById("root")!).render(<App />);
 })();
